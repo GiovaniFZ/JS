2977eed389a4f977864aa0ae7d5ee8e05b94f7f4
GiovaniFZ <giovanifinazzi@gmail.com>
2025-09-10 20:51:59 -0300

    chore: add coercion and basics

---
+++ age.js
@@ -0,0 +1,2 @@
+var a = prompt("Enter a number")
+console.log(a)
---
+++ coercion.js
@@ -0,0 +1,4 @@
+// Let's suppose we have a number and a string, they're not the same but can be coerced into each other
+var num = 5;
+var str = "5";
+console.log(num == str); // true because of type coercion
